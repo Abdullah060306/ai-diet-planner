461fce88633ebb93fd25179c805a78262989c063
Ch Abdullah Arshad <abdch2134@gmail.com>
2026-07-25 16:47:00 +0500

    Fix next.config.ts syntax

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['bcryptjs'],
   typescript: {
     ignoreBuildErrors: true,
   },
+};
+
+export default nextConfig;
